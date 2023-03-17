795eac8f086cd255bfc15fc8a99e61f119d86540
Leonardo Alcântara <leonardolucasga@hotmail.com>
2023-03-17 16:15:41 -0300

    Primeiro Desafio Finalizado

---
+++ Variaveis e Operadores/aula.js
@@ -0,0 +1,18 @@
+
+
+ /*let a = 10;
+a =20
+const pi= 3.14159;
+
+
+
+console.log('Hello Wolrd!')
+
+let a = 10;
+console.log(a)
+a = 20;
+console.log(a) 
+const a = 10;
+console.log(a)
+a = 20 ;
+console.log(a) */
---
+++ Variaveis e Operadores/primeirodesafio.js
@@ -0,0 +1,14 @@
+/* Faça um programa para calcular o preço de uma viagem;
+
+Você terá 3 variáveis, Sendo elas:
+Preço do combustível;
+Gasto medio de combustível do carro por KM
+Distância em KM da viagem; */
+
+const precoCombustivel = 5.79;
+const kmPorLitros = 10;
+const distEmKm = 100;
+
+const litroConsumidos = distEmKm /kmPorLitros;
+const precoViagem = precoCombustivel*litroConsumidos;
+console.log(`O preço que foi gasto foi de R$ ${precoViagem} `);
